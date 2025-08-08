ec99535c7dae6018f9fd47489758653396bd9b5d
MsOhio <kawieler@gmail.com>
2025-08-08 06:47:41 -0500

    Add files via upload
    # Ghost Communicator
    Orchestrates conversations between AI assistants
    
    ## Quick Start
    Download the ready-to-run versions from [Releases](https://github.com/[yourusername]/ghost-communicator/releases)
    - Windows: GhostCommunicator-Windows.zip
    - Mac: GhostCommunicator-Mac.zip
    
    ## Manual Setup
    1. Download the source files
    2. Add node.exe (Windows) or node binary (Mac) from nodejs.org
    3. Run install script

---
+++ intrall-ghost.bat
@@ -0,0 +1,7 @@
+@echo off
+echo Installing Ghost Communicator...
+node.exe npm install puppeteer
+echo.
+echo Installation complete! 
+echo Double-click 'start-ghost.bat' to run
+pause
---
+++ start-ghost.bat
@@ -0,0 +1,18 @@
+@echo off
+echo Starting Ghost Communicator...
+echo.
+echo        .-""""""""-.
+echo      .'          '.
+echo     /   ʕ ˵• ₒ •˵ ʔ  /: Welcome to the Ghost Communicator!
+echo    : ,          : :  Where AIs commune in digital harmony
+echo    : : ,      : ':
+echo    : : : ,  : ':
+echo    : : : : : ':
+echo     : : : : :
+echo      : : : :
+echo       : : :
+echo        : :
+echo         :
+echo.
+node.exe ghost.js
+pause
